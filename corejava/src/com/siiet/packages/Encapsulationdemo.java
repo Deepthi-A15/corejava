c5f6e855fa68b3a3b72ec05512326ba517473c7c
91830 <91830@LAPTOP-KK50V616>
2024-07-03 23:06:03 +0530

    commit

---
+++ corejava/src/com/siiet/packages/Encapsulationdemo.java
@@ -0,0 +1,13 @@
+package com.siiet.packages;
+
+public class Encapsulationdemo {
+	int serialnumber;
+	String name;
+	int age;
+	void show()
+	{
+		System.out.printf("serialnumber "+serialnumber +" "+ "name " + name + " age" + age);
+	}
+
+}
+ 
